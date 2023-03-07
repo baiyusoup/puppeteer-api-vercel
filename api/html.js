966f73f01b9bc805c62eb383db97a365fad4ab68
baiyusoup <1575727840@qq.com>
2023-03-07 23:37:51 +0800

    test

--- api/html.js
+++ api/html.js
@@ -12,26 +12,31 @@ const disabledMap = new Map(disabledResourceType);
 module.exports = async function handler(req, res) {
   const url = req.query.t;
   const wait = req.query.wait;
-  const browser = await setup();
-  console.log("testID=>", browser.__test_id__);
-  const page = await browser.newPage();
-  await page.setRequestInterception(true);
-  page.on('request', (request) => {
-    if (request._interceptionHandled) return;
-    if (disabledMap.has(request.resourceType())) {
-      request.abort();
-    } else {
-      request.continue();
+  try {
+    const browser = await setup();
+    console.log("testID=>", browser.__test_id__);
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+      if (request._interceptionHandled) return;
+      if (disabledMap.has(request.resourceType())) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    })
+    await page.goto(url);
+    if (wait) {
+      await page.waitForSelector(wait, { visible: true });
     }
-  })
-  await page.goto(url);
-  if (wait) {
-    await page.waitForSelector(wait, { visible: true });
+    const html = await page.evaluate(() => {
+      const target = document.body.querySelector(wait) || {};
+      return target.innerHTML;
+    });
+    page.close();
+    res.status(200).json({ html });
+  } catch (e) {
+    console.log(e);
+    res.status(200).json({ error: e });
   }
-  const html = await page.evaluate(() => {
-    const target = document.body.querySelector(wait) || {};
-    return target.innerHTML;
-  });
-  page.close();
-  res.status(200).json({ html });
 }
