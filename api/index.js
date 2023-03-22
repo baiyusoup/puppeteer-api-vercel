b88fcf853188bf671a78b24f4155ae61c1b78189
Baiyusoup <1575727840@qq.com>
2023-03-23 00:26:24 +0800

    test: 6

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
+const handleHtml = require("./html");
+/**
+ * @param {import('@vercel/node').VercelRequest} req
+ * @param {import('@vercel/node').VercelResponse} res
+ * @returns
+ */
 module.exports = async function handler(req, res) {
+  if (req.url.endsWith("/html")) {
+    return handleHtml(req, res);
+  }
   res.json({ name: "John Doe" });
 };
