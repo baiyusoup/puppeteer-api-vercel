890efcd7c6d86df3bab376a3bfe5d0f6d47151ae
Baiyusoup <1575727840@qq.com>
2023-03-23 00:51:30 +0800

    test: 8

--- api/index.js
+++ api/index.js
@@ -1,12 +1,56 @@
-const handleHtml = require("./html");
+const puppeteer = require("puppeteer-core");
+const cheerio = require("cheerio");
+const chrome = require("chrome-aws-lambda");
+
 /**
  * @param {import('@vercel/node').VercelRequest} req
  * @param {import('@vercel/node').VercelResponse} res
  * @returns
  */
-module.exports = async function handler(req, res) {
-  if (req.url.endsWith("/html")) {
-    return handleHtml(req, res);
+module.exports = async (req, res) => {
+  const slug = req?.query?.slug;
+  if (!slug) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ id: null }));
+    return;
   }
-  res.json({ name: "John Doe" });
+
+  const browser = await puppeteer.launch(
+    process.env.NODE_ENV === "production"
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {}
+  );
+
+  const page = await browser.newPage();
+  page.setUserAgent(
+    "Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10"
+  );
+  await page.goto(`https://m.youtube.com/${slug}/videos`);
+
+  let content = await page.content();
+  var $ = cheerio.load(content);
+  $.prototype.exists = function (selector) {
+    return this.find(selector).length > 0;
+  };
+
+  let id = null;
+  const isLive = $("body").exists('[data-style="LIVE"]');
+  if (isLive) {
+    const url = $("ytm-compact-video-renderer .compact-media-item-image").attr(
+      "href"
+    );
+    const arr = url.split("?v=");
+    id = arr[1];
+  }
+
+  await browser.close();
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ id }));
 };
